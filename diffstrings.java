bfabbd4f50985caf9e32eae19bfd782737529a03
hammadshah1833 <154410237+hammadshah1833@users.noreply.github.com>
2025-10-27 15:45:07 +0500

    First Commit

---
+++ diffstrings.java
@@ -0,0 +1,19 @@
+public class diffstrings {
+    public static void main(String[] args) {
+String s1="my";
+        String s2="name";
+        String s3="is";
+        String s4="Syed";
+        String s5="mustafa";
+
+        String s6=s1.concat(s2);
+        String s7=s6.concat(s3);
+        String s8=s7.concat(s4);
+        String s9=s8.concat(s5);
+        System.out.println(s9);
+        String ss=s4.toUpperCase();
+        System.out.println(ss);
+        String sub=s4.substring(1,3);
+        System.out.println(sub);
+
+}}
